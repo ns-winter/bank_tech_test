0f1ee5ebb4002713512c1154cbc3905c7d5ebfa3
Nigel Said <nigelsaid@gmail.com>
2017-06-12 14:26:29 +0100

    adds Withdraw function and validation checks on transactions

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,4 @@
+require 'date'
 # BankAccount class
 class BankAccount
   attr_reader :balance
@@ -6,7 +7,30 @@ class BankAccount
     @balance = 0
   end
 
-  def deposit(amount)
+  def deposit(amount, date)
+    valid_transaction_test(amount, date)
     @balance += amount
+  end
+
+  def withdraw(amount, date)
+    valid_transaction_test(amount, date)
+    @balance -= amount
+  end
+
+  private
+
+  def valid_transaction_test(amount, date)
+    confirm_valid_date(date)
+    confirm_valid_amount(amount)
+  end
+
+  def confirm_valid_date(date)
+    Date.strptime(date, '%d/%m/%Y')
+  end
+
+  def confirm_valid_amount(amount)
+    unless amount.is_a? Numeric
+      raise 'amount provided is not valid'
+    end
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -12,6 +12,19 @@ describe BankAccount do
   end
 
   it 'can deposit £100' do
-    expect { subject.deposit(100) }.to change { subject.balance }.by(100)
+    expect { subject.deposit(100, '12/07/2017') }.to change { subject.balance }.by(100)
+  end
+
+  it 'can withdraw £50' do
+    subject.deposit(100, '12/07/2017')
+    expect { subject.withdraw(50, '12/07/2017') }.to change { subject.balance }.by(-50)
+  end
+
+  it 'raises an error if given incorrect amount argument' do
+    expect { subject.deposit('bad amount', '12/07/2017') }.to raise_error 'amount provided is not valid'
+  end
+
+  it 'raises an error if given an incorrect date argument' do
+    expect { subject.withdraw(100, 'bad date') }.to raise_error 'invalid date'
   end
 end
