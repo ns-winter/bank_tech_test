cc32b230871964d978ce81c12a18bcb61479741c
Nigel Said <nigelsaid@gmail.com>
2017-06-13 15:35:51 +0100

    refactors transaction_record_spec.rb

--- spec/transaction_record_spec.rb
+++ spec/transaction_record_spec.rb
@@ -5,15 +5,21 @@ describe TransactionRecord do
   first_record = { date: '12/06/2017', credit: '100.00', debit: nil, balance: '500.00' }
   second_record = { date: '13/06/2017', credit: nil, debit: '200.00', balance: '300.00' }
 
-  it 'starts with an empty record' do
-    expect(subject.history).to eq([])
+  describe '#history' do
+    it 'starts with an empty record' do
+      expect(subject.history).to eq([])
+    end
   end
 
-  it 'records a deposit' do
-    expect { subject.record_deposit('12/06/2017', 100, 500) }.to change { subject.history }.from([]).to([first_record])
+  describe '#record_deposit' do
+    it 'records a deposit' do
+      expect { subject.record_deposit('12/06/2017', 100, 500) }.to change { subject.history }.from([]).to([first_record])
+    end
   end
 
-  it 'records a withdrawal' do
-    expect { subject.record_withdrawal('13/06/2017', 200, 300) }.to change { subject.history }.from([first_record]).to([first_record, second_record])
+  describe '#record_withdrawal' do
+    it 'records a withdrawal' do
+      expect { subject.record_withdrawal('13/06/2017', 200, 300) }.to change { subject.history }.from([first_record]).to([first_record, second_record])
+    end
   end
 end
