4912668c9b66340f755a83dd93bf88c2edd00102
Nigel Said <nigelsaid@gmail.com>
2017-06-13 15:38:02 +0100

    refactors transaction_validator_spec.rb

--- spec/transaction_validator_spec.rb
+++ spec/transaction_validator_spec.rb
@@ -3,19 +3,25 @@ require 'transaction_validator'
 describe TransactionValidator do
   subject = described_class.new
 
-  it 'passes a valid transaction' do
-    expect { subject.validate(100, '12/07/2017') }.not_to raise_error
+  describe '#validate' do
+    it 'passes a valid transaction' do
+      expect { subject.validate(100, '12/07/2017') }.not_to raise_error
+    end
   end
 
-  it 'raises an error if given negative amount argument' do
-    expect { subject.validate(-100, '12/07/2017') }.to raise_error 'amount provided is not valid'
+  describe '#validate(confirm_valid_amount)' do
+    it 'raises an error if given negative amount argument' do
+      expect { subject.validate(-100, '12/07/2017') }.to raise_error 'amount provided is not valid'
+    end
+
+    it 'raises an error if given an incorrect date argument' do
+      expect { subject.validate(100, 'bad date') }.to raise_error 'invalid date'
+    end
   end
 
-  it 'raises an error if given an incorrect date argument' do
-    expect { subject.validate(100, 'bad date') }.to raise_error 'invalid date'
-  end
-
-  it 'raises an error if given non-numeric amount argument' do
-    expect { subject.validate('bad amount', '12/07/2017') }.to raise_error 'amount provided is not valid'
+  describe '#validate(confirm_valid_date)' do
+    it 'raises an error if given non-numeric amount argument' do
+      expect { subject.validate('bad amount', '12/07/2017') }.to raise_error 'amount provided is not valid'
+    end
   end
 end
