33fce1652192608b9af712b97532202465fe1b39
Nigel Said <nigelsaid@gmail.com>
2017-06-13 15:35:08 +0100

    refactors bank_account_spec.rb

--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -3,20 +3,26 @@ require 'bank_account'
 describe BankAccount do
   subject = described_class.new
 
-  it 'has a balance' do
-    expect(subject).to respond_to(:balance)
+  describe '#balance' do
+    it 'has a balance' do
+      expect(subject).to respond_to(:balance)
+    end
+
+    it 'starting balance is 0' do
+      expect(subject.balance).to eq(0)
+    end
   end
 
-  it 'starting balance is 0' do
-    expect(subject.balance).to eq(0)
+  describe '#credit_account' do
+    it 'can credit the account by £100' do
+      expect { subject.credit_account(100) }.to change { subject.balance }.by(100)
+    end
   end
 
-  it 'can credit the account by £100' do
-    expect { subject.credit_account(100) }.to change { subject.balance }.by(100)
-  end
-
-  it 'can debit the account £50' do
-    subject.debit_account(100)
-    expect { subject.debit_account(50) }.to change { subject.balance }.by(-50)
+  describe '#debit_account' do
+    it 'can debit the account £50' do
+      subject.debit_account(100)
+      expect { subject.debit_account(50) }.to change { subject.balance }.by(-50)
+    end
   end
 end
